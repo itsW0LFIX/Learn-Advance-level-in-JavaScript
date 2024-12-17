2a1220249a626ceab0c5bd9ba25fe6a35ea731b8
itsW0LFIX <devopscode9@email.com>
2024-12-17 19:21:50 +0100

    Exercice 4 les tests (TD L1algorithmique)

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-
 let robot = "Machine : "
 
  console.log(robot + "Write Number : ")
 
- let nb = prompt("number : ");
+ let nb1 = prompt("number 1 : ");
+ let nb2 = prompt("number 2 : ");
 
- if ( nb > 0) {
-    console.log(robot + "the number positif ");
- }else if (nb < 0){
-    console.log(robot + "the number négatif ");
+ if ( nb1 + nb2 > 0) {
+    console.log(robot + "the numbers positif ");
+ }else if (nb1 + nb2 < 0) {  
+    console.log(robot + "the numbers négatif ");
  }else{
     console.log(robot + "the number is 0 ");
  }
