5abdd1c7ea7ee9be5f4e1efae0fc70a6e3a5bd38
itsW0LFIX <devopscode9@email.com>
2024-12-21 18:45:12 +0100

    Exercice 3 Les boucles (TD L1algorithmique)

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-let robot = "Machine : ",nbofrobot= 13;
+let robot = "Machine : ",nbofrobot= 10;
 
  console.log(robot + "Write Number 1 to 3 : ")
-
- while (true) { // Loop runs indefinitely
-    let nb = prompt("Number: "); // Prompt the user for a number
-    nb = parseInt(nb, 10); // Convert the input to a number
-
-    if (nb > nbofrobot) { // Check if the guessed number is correct
-        console.log(robot + "the number is small");
-         // Exit the loop (close it)
-    } else if(nb < nbofrobot){
-        console.log(robot + "the number big");
-    }else if (nb === nbofrobot) {
-                console.log("the number is :"+nb);
-        break;
-    }
+ let nb = prompt("Number: "); // Prompt the user for a number
+ nb = parseInt(nb, 10); // Convert the input to a number
+ 
+ for (let i = 1; i <= 10; i++) {
+    console.log(nb + i);
 }
