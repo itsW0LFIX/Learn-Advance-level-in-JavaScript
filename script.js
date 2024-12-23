2d28cbcc67d7a91cfd859ccf1665b3e700509b38
itsW0LFIX <devopscode9@email.com>
2024-12-23 19:59:15 +0100

    Exercise 8 Loops (TD L1algorithmic)

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let nb = 3,save = 0;
+let nb,save = 0;
 
-for (let i = 0; i < nb; i++) {
-    save += nb * i;
+for (let i = 1; i <= 5; i++) {
+    let nb = prompt("Number "+i+" : "); 
+    nb = parseInt(nb, 10); 
+    if (nb >= save) {
+        save = nb;
+    } else {
+        console.log("skip") // just for testing
+    } 
 }
-console.log(nb+"! = "+ save);
+console.log(save);
